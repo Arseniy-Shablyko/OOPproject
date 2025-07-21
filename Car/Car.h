9873e0db3fe52e82e7ce6791a5d472de6f9f7226
Николай Шаблыко <nick211278@gmail.com>
2025-07-21 13:57:01 +0300

    Car project added, car class added

---
+++ Car/Car.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "libraries.h"
+class Car
+{
+private:
+	string brand;
+	string model;
+	int age;
+	double price;
+public:
+	Car();
+	Car(string brand, string model);
+	Car(string brand, string model, int age, double price);
+	~Car();
+
+	string getBrand();
+	void setBrand(string brand);
+
+	string getModel();
+	void setModel(string model);
+
+	int getAge();
+	void setAge(int age);
+
+	double getPrice();
+	void setPrice(double price);
+
+	string toInfo();
+};
+
